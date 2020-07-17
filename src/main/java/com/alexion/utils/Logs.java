c907a9158209bc39a031fa7425175407ba69ee77
naveen.kumar.meduri <naveen.kumar.meduri@.accenture.com>
2020-07-17 15:58:03 +0530

    second commit- changed log4j2.xml and pom file

--- src/main/java/com/alexion/utils/Logs.java
+++ src/main/java/com/alexion/utils/Logs.java
@@ -1,15 +1,15 @@
 package com.alexion.utils;
 
-import org.apache.log4j.Logger;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 
 public class Logs {
-    public static final String ASTRIC_DESIGN ="****************************************************************************************";
+	public static final String ASTRIC_DESIGN = "****************************************************************************************";
 	// Initialize Log4j logs
-	static Logger Log = Logger.getLogger(Logs.class.getName());//
-    	
+	private static final Logger Log = LogManager.getLogger(Logs.class.getName());
 	// This is to print log for the beginning of the test case, as we usually run so
 	// many test cases as a test suite
-    
+
 	public static void startTestCase(String sTestCaseName) {
 
 		Log.info(ASTRIC_DESIGN);
@@ -19,7 +19,7 @@ public class Logs {
 		Log.info("$$$$$$$$$$$$$$$$$$$$$                 " + sTestCaseName + "       $$$$$$$$$$$$$$$$$$$$$$$$$");
 
 		Log.info(ASTRIC_DESIGN);
-		
+
 		Log.info(ASTRIC_DESIGN);
 
 	}
@@ -28,7 +28,8 @@ public class Logs {
 
 	public static void endTestCase(String sTestCaseName) {
 
-		Log.info("XXXXXXXXXXXXXXXXXXXXXXX             " + "-E---N---D-" + sTestCaseName+ "           XXXXXXXXXXXXXXXXXXXXXX");
+		Log.info("XXXXXXXXXXXXXXXXXXXXXXX             " + "-E---N---D-" + sTestCaseName
+				+ "           XXXXXXXXXXXXXXXXXXXXXX");
 
 		Log.info("X");
 
@@ -43,5 +44,5 @@ public class Logs {
 	public static void info(String message) {
 
 		Log.info(message);
-      }
+	}
 }
